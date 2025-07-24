5339f6011607099ae27e719146db358f692b37d0
Loena Nascimento Da Silva <loda-sil@c3r4p5.42sp.org.br>
2025-07-24 18:20:57 -0300

    feat: add ft_memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: loda-sil <loda-sil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/24 15:30:18 by loda-sil          #+#    #+#             */
+/*   Updated: 2025/07/24 18:02:30 by loda-sil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char *ptr_dest;
+	unsigned const char *ptr_src;
+
+	ptr_dest = (unsigned char *)dest;
+	ptr_src = (unsigned const char *)src;
+	while (n--)
+		*ptr_dest++ = *ptr_src++;
+	return (dest);
+}
