309f701e7568d8cea14cf0561f5cd087f6256d01
Loena Nascimento Da Silva <loda-sil@c3r4p5.42sp.org.br>
2025-07-24 18:19:46 -0300

    refactor: add new functions

--- libft.h
+++ libft.h
@@ -6,19 +6,25 @@
 /*   By: loda-sil <loda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 11:12:35 by loda-sil          #+#    #+#             */
-/*   Updated: 2025/07/22 16:27:48 by loda-sil         ###   ########.fr       */
+/*   Updated: 2025/07/24 18:12:47 by loda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
 
-#include <unistd.h>
-#include <stdlib.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdlib.h>
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 
 #endif
-
