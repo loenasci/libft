3b4bb88d581b8024c404f17e3c3ffb0948f49901
Loena Nascimento Da Silva <loda-sil@c2r3p6.42sp.org.br>
2025-07-28 18:05:10 -0300

    feat: add ft_memmove

---
+++ ft_memmove.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: loda-sil <loda-sil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/28 12:37:20 by loda-sil          #+#    #+#             */
+/*   Updated: 2025/07/28 17:26:07 by loda-sil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	if (dest < src)
+		return (ft_memcpy(dest, src, n));
+	while (n--)
+		((unsigned char *)dest)[n] = ((unsigned char *)src)[n];
+	return (dest);
+}
