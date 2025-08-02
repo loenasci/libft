3f40587893cd4362e3130c34c2494577fa9b175e
Loena Nascimento <loenascimentosilva@gmail.com>
2025-08-02 15:06:11 -0300

    feat: add ft_memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: loena <loena@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/02 14:29:24 by loena             #+#    #+#             */
+/*   Updated: 2025/08/02 15:05:45 by loena            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*ptr1;
+	unsigned char	*ptr2;
+
+	ptr1 = (unsigned char *)s1;
+	ptr2 = (unsigned char *)s2;
+	while (n--)
+	{
+		if (*ptr1 != *ptr2)
+			return ((int)*ptr1 - (int)*ptr2);
+		ptr1++;
+		ptr2++;
+	}
+	return (0);
+}
