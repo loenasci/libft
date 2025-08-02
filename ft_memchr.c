da32bb490f5ae1f50001fba6a5b50249055e171f
Loena Nascimento <loenascimentosilva@gmail.com>
2025-08-02 14:28:39 -0300

    feat: add ft_memchr

---
+++ ft_memchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: loena <loena@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/02 13:56:35 by loena             #+#    #+#             */
+/*   Updated: 2025/08/02 14:27:58 by loena            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*str;
+
+	str = (unsigned char *)s;
+	while (n--)
+	{
+		if (*str == c)
+			return ((void *)str);
+		str++;
+	}
+	return (NULL);
+}
