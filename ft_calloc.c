bd44776c28f69fefaac0013f80e1ad2edcb0e5a1
Loena Nascimento Da Silva <loda-sil@c3r6p3.42sp.org.br>
2025-08-04 16:24:34 -0300

    refactor: add norminette

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,20 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: loena <loena@student.42.fr>                +#+  +:+       +#+        */
+/*   By: loda-sil <loda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/03 22:19:17 by loena             #+#    #+#             */
-/*   Updated: 2025/08/03 23:03:07 by loena            ###   ########.fr       */
+/*   Updated: 2025/08/04 16:06:41 by loda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_calloc(size_t nmemb, size_t size)
+void	*ft_calloc(size_t nmemb, size_t size)
 {
 	unsigned char	*ptr;
 	size_t			total;
-	
+
 	if (nmemb != 0 && size > (size_t)-1 / nmemb)
 		return (NULL);
 	total = nmemb * size;
