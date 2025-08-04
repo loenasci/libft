8da587c1693d2c29a9fbc3145fa6e245de8a1b41
Loena Nascimento Da Silva <loda-sil@c3r6p3.42sp.org.br>
2025-08-04 14:49:07 -0300

    refactor: organizes the header and adds more functions

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: loda-sil <loda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 11:12:35 by loda-sil          #+#    #+#             */
-/*   Updated: 2025/07/28 17:26:46 by loda-sil         ###   ########.fr       */
+/*   Updated: 2025/08/04 14:47:16 by loda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,42 @@
 
 # include <unistd.h>
 # include <stdlib.h>
-# include <stdlib.h>
 
+/*
+** Character checks
+*/
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-void	*ft_memset(void *s, int c, size_t n);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+
+/*
+** Memory manipulation
+*/
 void	ft_bzero(void *s, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	ft_calloc(size_t nmemb, size_t size);
 
+/*
+** String manipulation
+*/
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+
+/*
+** Conversion
+*/
+int		ft_atoi(const char *nptr);
 #endif
