b833391c7c51ffada22307984870cdc22e49ce6f
Loena Nascimento Da Silva <loda-sil@c3r6p3.42sp.org.br>
2025-08-04 16:23:58 -0300

    fix: fix ft_calloc prototype

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: loda-sil <loda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 11:12:35 by loda-sil          #+#    #+#             */
-/*   Updated: 2025/08/04 14:47:16 by loda-sil         ###   ########.fr       */
+/*   Updated: 2025/08/04 16:07:47 by loda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
-void	ft_calloc(size_t nmemb, size_t size);
+void	*ft_calloc(size_t nmemb, size_t size);
 
 /*
 ** String manipulation
