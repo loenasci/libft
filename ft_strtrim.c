4d72310ac6b703bb1fd0ce75d27668029f900b65
Loena Nascimento Da Silva <loda-sil@c1r2p1.42sp.org.br>
2025-08-05 16:37:08 -0300

    feat: add ft_strtrim

---
+++ ft_strtrim.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: loda-sil <loda-sil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/05 12:30:10 by loda-sil          #+#    #+#             */
+/*   Updated: 2025/08/05 16:13:09 by loda-sil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*trimmed;
+	int		start;
+	int		end;
+
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	while (ft_strchr(set, s1[start]) && start <= end)
+		start++;
+	if (start > end)
+		return (ft_strdup(""));
+	while (ft_strchr(set, s1[end]) && end >= 0)
+		end--;
+	trimmed = (char *)ft_calloc(end - start + 2, sizeof(char));
+	if (!trimmed)
+		return (NULL);
+	ft_strlcpy(trimmed, &s1[start], end - start + 2);
+	return (trimmed);
+}
