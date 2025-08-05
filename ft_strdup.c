54d59fbcf372bcc0d19d3ab904910046f0a5992e
Loena Nascimento Da Silva <loda-sil@c1r2p1.42sp.org.br>
2025-08-05 16:36:10 -0300

    refactor: change malloc to ft_calloc for memory allocation

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: loda-sil <loda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 09:36:56 by loda-sil          #+#    #+#             */
-/*   Updated: 2025/08/05 10:05:32 by loda-sil         ###   ########.fr       */
+/*   Updated: 2025/08/05 16:12:23 by loda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_strdup(const char *s)
 {
 	char	*str;
 
-	str = malloc((ft_strlen(s) + 1) * sizeof(char));
+	str = (char *)ft_calloc((ft_strlen(s) + 1), sizeof(char));
 	if (!str)
 		return (NULL);
 	ft_memcpy(str, s, ft_strlen(s) + 1);
