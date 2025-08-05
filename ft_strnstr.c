d330cc7a03d704cbaee2cf9a8ea0e00fdc5ca8f0
Loena Nascimento Da Silva <loda-sil@c1r2p1.42sp.org.br>
2025-08-05 11:16:04 -0300

    refactor: remove unnecessary assignment from variable j

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: loda-sil <loda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/02 15:07:44 by loena             #+#    #+#             */
-/*   Updated: 2025/08/04 16:05:05 by loda-sil         ###   ########.fr       */
+/*   Updated: 2025/08/05 11:15:03 by loda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	j;
 
 	i = 0;
-	j = 0;
 	if (!little[i])
 		return ((char *)big);
 	while (big[i] && i < len)
