d2a383f5e938c7aa0a759c47bafdfcd842123da0
Loena Nascimento Da Silva <loda-sil@c3r3p1.42sp.org.br>
2025-08-07 16:24:21 -0300

    fix: check for zero size and return empty string

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: loda-sil <loda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/03 22:19:17 by loena             #+#    #+#             */
-/*   Updated: 2025/08/05 12:00:39 by loda-sil         ###   ########.fr       */
+/*   Updated: 2025/08/07 16:20:36 by loda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	unsigned char	*ptr;
 	size_t			total;
 
+	if (!size)
+		return (ft_strdup(""));
 	if (nmemb != 0 && size > (size_t)-1 / nmemb)
 		return (NULL);
 	total = nmemb * size;
