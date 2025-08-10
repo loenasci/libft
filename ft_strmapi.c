807d10d0b0989e66563c60b6c9c16eb5100d380a
Loena Nascimento <loenascimentosilva@gmail.com>
2025-08-10 16:19:56 -0300

    feat: add ft_strmap

---
+++ ft_strmapi.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: loena <loena@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/10 15:40:13 by loena             #+#    #+#             */
+/*   Updated: 2025/08/10 16:18:06 by loena            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char *result;
+	size_t len;
+	size_t i;
+	
+	if (!s || !f)
+		return (NULL);
+	len = ft_strlen(s);
+	result = (char *)ft_calloc(len + 1, sizeof(char));
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		result[i] = f(i, s[i]);
+		i++;
+	}
+	return (result);
+}
