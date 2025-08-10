da0d5962594d35273e986c8cf94df24e5e2b2bb4
Loena Nascimento <loenascimentosilva@gmail.com>
2025-08-10 11:47:20 -0300

    feat: add ft_itoa

---
+++ ft_itoa.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: loena <loena@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/10 10:38:56 by loena             #+#    #+#             */
+/*   Updated: 2025/08/10 11:46:33 by loena            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+int	count_digits(int n)
+{
+	int 		count;
+	long int	nbr;
+
+	if (n == 0)
+		return (1);
+	count = 0;
+	nbr = n;
+	if (nbr < 0)
+	{
+		nbr = -nbr;
+		count++;
+	}
+	while (nbr > 0)
+	{
+		nbr = nbr / 10;
+		count++;
+	}
+	return (count);
+}
+
+char	*ft_itoa(int n)
+{
+	char		*str;
+	long int	nb;
+	int			digits;
+
+	nb = n;
+	digits = count_digits(n);
+	str = (char *)ft_calloc(digits + 1, sizeof(char));
+	if (!str)
+		return (NULL);
+	if (nb == 0)
+		str[0] = 48;
+	if (nb < 0)
+	{
+		str[0] = '-';
+		nb = nb * -1;
+	}
+	digits -= 1;
+	while (nb > 0)
+	{
+		str[digits--] = nb % 10 + 48;
+		nb = nb / 10;
+	}
+	return (str);
+}
