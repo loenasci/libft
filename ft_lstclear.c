a9e0fd1643067d2b1c2462719e7d4c2da2dc932a
Loena Nascimento <loenascimentosilva@gmail.com>
2025-08-18 15:22:48 -0300

    feat: add ft_lstclear

---
+++ ft_lstclear.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: loena <loena@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/15 17:59:44 by loena             #+#    #+#             */
+/*   Updated: 2025/08/18 15:11:10 by loena            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*current;
+	t_list	*next_node;
+
+	if (!lst || !del)
+		return ;
+	current = *lst;
+	while (current)
+	{
+		next_node = current->next;
+		del(current->content);
+		free(current);
+		current = next_node;
+	}
+	*lst = NULL;
+}
